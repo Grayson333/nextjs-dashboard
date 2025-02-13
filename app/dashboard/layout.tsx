b568ac2a65e91389663c214dda4eea8826c79f4c
Grayson333 <graysonli1024@gmail.com>
2025-02-13 20:43:57 +0800

    Update 4

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,16 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+// import { useEffect } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    // useEffect(() => {
+    //     console.log('WKS-9999 3');
+    //     return () => {
+    //         console.log('WKS-9999 4');
+    //     }
+    // }, []);
+
+    // console.log('WKS-9999 2');
+    
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
